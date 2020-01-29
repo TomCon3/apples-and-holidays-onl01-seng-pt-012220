378229aa826911e0badde492baaf1a0ac15fc162
Tommy Connor <tconnor98@aol.com>
2020-01-29 21:34:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, data|
-  puts "#{season.to_s.capitalize"
+  puts "#{season.to_s.capitalize!}:"
   data.each do |
   end
 end

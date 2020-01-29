03729a1221dcb92ecef5dd5ec75a39ec157b0152
Tommy Connor <tconnor98@aol.com>
2020-01-29 21:35:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, data|
   puts "#{season.to_s.capitalize!}:"
   data.each do |holiday, supply|
-    array = holiday.to_s
+    array = holiday.to_s.split
   end
 end
 end

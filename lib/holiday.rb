ad59eae955a95359fe321644b73d5aa362e09f33
Tommy Connor <tconnor98@aol.com>
2020-01-29 21:30:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,8 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, data|
   data.each do |holiday, supply|
     holiday.values.flatten
+  end
+end
 end
 
 def all_holidays_with_bbq(holiday_hash)
